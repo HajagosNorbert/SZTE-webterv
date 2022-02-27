50e2706a76a55a9e8dc876632545d72aeccf9e4a
HajagosNorbert <hajagosnorbi@gmail.com>
2022-02-27 19:16:08 +0100

    Responsive Menubar added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SZTE-webterv projektmunka
+
+Iskolai, oktatók -> rendszergazdáknak
+login, rendszergazda megnézni a hibákat.
+
+Én csinálom a navigációt, Isti a bejelentkezést.
---
+++ scripts/navbar.js
@@ -0,0 +1,22 @@
+const toggleButton = document.getElementsByClassName("navbar-toggle")[0];
+const navbarLinks = document.getElementsByClassName("navbar-link");
+const toggleIcon = toggleButton.childNodes[0];
+
+function flipMenuIconWithCloseIcon() {
+  if (toggleIcon.classList.contains("fa-bars")) {
+    toggleIcon.classList.remove("fa-bars");
+    toggleIcon.classList.add("fa-times");
+  } else {
+    toggleIcon.classList.remove("fa-times");
+    toggleIcon.classList.add("fa-bars");
+  }
+}
+
+toggleButton.addEventListener("click", () => {
+  flipMenuIconWithCloseIcon();
+
+  const navbarLinkArray = Array.from(navbarLinks);
+  navbarLinkArray.forEach((link) => {
+    link.classList.toggle("active");
+  });
+});
